9b41107cdc7eac0f06f51b296a6b002d9318c319
Akın Can HANÇER <akncntr57@gmail.com>
2026-02-13 02:52:48 +0300

    feat: Create GameManager, InputReader classes.

---
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+
+        }
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Inputs/InputReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Project.Scripts.Inputs
+{
+    public class InputReader : MonoBehaviour, IA_Character.IMovementActions
+    {
+        private IA_Character _controls;
+
+        private void Awake()
+        {
+            _controls = new IA_Character();
+            _controls.Movement.SetCallbacks(this);
+            _controls.Movement.Enable();
+        }
+
+        private void OnDestroy()
+        {
+            _controls?.Dispose();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnMove");
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnLook");
+        }
+
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnCrouch");
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnJump");
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnRun");
+        }
+    }
+}
